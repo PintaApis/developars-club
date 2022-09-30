fa81af3e9f72cd260bb88998c782d9d959292c01
Carlos Azaustre <cazaustre@gmail.com>
2022-09-30 22:17:44 +0200

    build(app/web): :construction_worker: test the CD flow (#7)
    closes #6

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,9 @@
-import { Button } from "ui";
-
-export default function Web() {
+export default function Home() {
   return (
     <div>
       <h1>Developars.club</h1>
-      <Button />
+      <p>Esto es un MVP muy <em>Minimum</em></p>
+      <p>Hecho con ❤️ por la <a href="">PintaAPis Developer Community</a></p>
     </div>
   );
 }
