45e1b0d46609034eadb172ed1c964751586d72eb
Carlos Azaustre <cazaustre@gmail.com>
2022-10-01 15:32:33 +0200

    test(app/web): :test_tube: add acceptance test for home page

--- apps/web/cypress/e2e/home.cy.js
+++ apps/web/cypress/e2e/home.cy.js
@@ -3,4 +3,16 @@ describe("Developars.club Home feed", function () {
     cy.visit("/");
     cy.contains("h1[data-test='heading']", "Developars.club");
   });
+
+  it("shows a developer profile list", function () {
+    cy.visit("/");
+    cy.get('[data-test="developer-profile-list"]').should("exist");
+    cy.get('[data-test="developer-profile"]').should((items) => {
+      expect(items).to.have.length(3);
+      const names = [...items].map(
+        (item) => item.querySelector("h2").innerHTML
+      );
+      expect(names).to.deep.eq(["John Doe", "Jane Doe", "John Smith"]);
+    });
+  });
 });
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,12 @@ export default function Home() {
     <div>
       <h1 data-test="heading">Developars.club</h1>
       <p>Esto es un MVP muy <em>Minimum</em></p>
-      <p>Hecho con ❤️ por la <a href="">PintaAPis Developer Community</a></p>
+      <div data-test="developer-profile-list">
+        <div data-test="developer-profile"><h2>John Doe</h2></div>
+        <div data-test="developer-profile"><h2>Jane Doe</h2></div>
+        <div data-test="developer-profile"><h2>John Smith</h2></div>
+      </div>
+      <p>Hecho con ❤️ por la <a href="https://discord.gg/carlosazaustre">PintaAPis Developer Community</a></p>
     </div>
   );
 }
